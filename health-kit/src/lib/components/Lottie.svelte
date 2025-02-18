3cfd7ceaa9abb3848a98f3c2903e8aad447d4f31
LukasJurena <jurenalukash@gmail.com>
2025-02-18 07:47:57 +0100

    first commit

---
+++ health-kit/src/lib/components/Lottie.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+</script>
+
+<LottiePlayer
+  src='https://assets2.lottiefiles.com/packages/lf20_wxUJzo.json'
+  autoplay={true}
+  loop={true}
+  controls={true}
+  renderer='svg'
+  background='transparent'
+  height={500}
+  width={500}
+  controlsLayout={[
+		"previousFrame",
+		"playpause",
+		"stop",
+		"nextFrame",
+		"progress",
+		"frame",
+		"loop",
+		"spacer",
+		"background",
+		"snapshot",
+		"zoom",
+		"info"
+	]} />
